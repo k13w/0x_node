34a7e0b65c7a39b87145924a1cd067a358cd77f7
Moon <gilmarheavensilva@hotmail.com>
2018-10-04 02:23:19 -0300

    :dolphin:

--- src/app/views/js/index.js
+++ src/app/views/js/index.js
@@ -1,32 +1,29 @@
 $(document).ready(function() {
   $('.login').click(function() {
-    if ($('input:password').val() !== "") {
-      
-      var name = $('.name').val();
-      var email = $('.email').val();
-      var password = $('.password').val();
-
-
-      var payload = {
-          name: name,
-          email: email,
-          password: password
-      }
-
-      var url = 'http://localhost:7000';
-      $.ajax({
-        url:url + '/auth/register',
-        type:"POST",
-        data:payload,
-        contentType:"application/json; charset=utf-8",
-        dataType:"json",
-        success: function() { 
-          console.log(error)
-        }
-        })
-
-    } else {
-      alert('Password wrong, please, try again!');
+  if ($('input:password').val() !== "") {
+  
+    var name = $('.name').val();
+    var email = $('.email').val();
+    var password = $('.password').val();
+  
+  
+    var payload = {
+      name: name,
+      email: email,
+      password: password
     }
-  });
+  
+    var url = 'http://localhost:7000/';
+    axios.post(`${url}auth/register`, payload)
+    .then( response => {
+    console.log(response)
+  })
+    .catch( error => {
+    console.log(error)
+  })
+  
+  } else {
+    alert('Password wrong, please, try again!');
+  }
+});
 });
